3d8102d36ac7040367bbf545465973792b300898
Amey <130839577+ameynagulwar@users.noreply.github.com>
2023-06-15 20:13:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let btn = document.getElementById("enterBtn");
+      let para = document.getElementById("status");
+
+      btn.addEventListener("click",() => {
+        // para.innerHTML = "";
+         para.innerHTML = "<h1>"+"Entered Metaverse"+"</h1>"
+      })
